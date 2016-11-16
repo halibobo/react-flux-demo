a6d5347197ea8443f9cc247468ad5029e2536d7b
dahei <unitygames@163.com>
2016-11-16 14:15:08 +0800

    react basic test

--- apps/flux/index.js
+++ apps/flux/index.js
@@ -4,12 +4,34 @@
 
 import React from 'react'
 import {Relax, msg} from 'iflux2'
+import {fromJS} from 'immutable'
+
+
+const noop = () => {
+};
 
 @Relax
 export default class Main extends React.Component {
 
     static defaultProps = {
-        appShowTip: noop,
-    };
+        onShowTip: noop,
+    }
+
+    handleClick = (tip) =>{
+        onShowTip(tip);
+    }
+
+    render() {
+
+        // 目前微信支查询自营商品fromOwner=1
+        return (
+            <div className="home" style={{minHeight: 100}}>
+                <div
+                    onShowTip={this.props.onShowTip}>
+                    <input type="button" value="Focus the text input" onClick={this.handleClick("aa")} />
+                </div>
+            </div>
+        )
+    }
 
 };
--- apps/flux/store.js
+++ apps/flux/store.js
@@ -1,11 +1,19 @@
 /**
  * Created by su on 2016/11/14.
  */
-import {FluxActor} from './actor/actor'
+import FluxActor from './actor/actor'
+import {Store} from 'iflux2'
 
 
 export default class AppStore extends Store {
-    appShowTip = (tip) => {
+
+    bindActor() {
+        return [
+            new FluxActor
+        ]
+    }
+
+    onShowTip = (tip) => {
         this.dispatch('showTip', tip);
     };
 
